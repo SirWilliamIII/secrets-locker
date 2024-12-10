a04e75d13bc60e6f5b2fc378170097b8e3ed87bf
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-12-09 22:14:12 -0600

    re-add boto3

--- app/secret_locker.py
+++ app/secret_locker.py
@@ -6,6 +6,13 @@ from os import urandom
 
 kms_client = boto3.client("kms", region_name=os.getenv("AWS_REGION", "us-east-1"))
 
+session = boto3.Session(
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION", "us-east-1")
+)
+print("AWS Session created successfully!")
+
 class SecretLocker:
 	def __init__(self, key: bytes):
 		self.key = key
